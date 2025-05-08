4a26e69486376fe0ec831054d89fd1ae25f99561
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2025-05-08 21:29:17 +0200

    Init project

---
+++ src/main/java/dev/mikablondo/hibernate_reactive_test/HibernateReactiveTestApplication.java
@@ -0,0 +1,13 @@
+package dev.mikablondo.hibernate_reactive_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateReactiveTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateReactiveTestApplication.class, args);
+	}
+
+}
